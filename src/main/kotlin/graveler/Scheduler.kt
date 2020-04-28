43a11412c4b71ffdf3129db1964d0df717e4cbfb
skippi <jbtcao@gmail.com>
2020-04-27 22:31:40 -0500

    Removed cooldown from scheduler
    - Cooldown was useless for its purpose.
    - Removed print statement in scheduler.

--- src/main/kotlin/graveler/Scheduler.kt
+++ src/main/kotlin/graveler/Scheduler.kt
@@ -7,7 +7,6 @@ import net.minecraft.world.World
 @OptIn(ExperimentalStdlibApi::class)
 class Scheduler(private var processingRate: Double = 1.0) {
   private val queue = ArrayDeque<Action>()
-  private var cooldown: Int = 0
 
   fun schedule(action: Action): Scheduler {
     queue.addLast(action)
@@ -17,19 +16,12 @@ class Scheduler(private var processingRate: Double = 1.0) {
   fun tick(world: World): Scheduler {
     var count = 0.0
     val context = ActionContext(this, world)
-    while (!isBusy && queue.isNotEmpty() && count < processingRate) {
+    while (queue.isNotEmpty() && count < processingRate) {
       val action = queue.removeFirst()
       action.apply(context)
       count += action.weight
     }
 
-    cooldown = (cooldown + 1) % 1
-
-    println(queue.size)
-
     return this
   }
-
-  private val isBusy: Boolean
-    get() = cooldown != 0
 }
