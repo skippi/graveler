157ae81d7e2f6283508ce64ce48cbc67589f4b31
skippi <jbtcao@gmail.com>
2026-05-28 17:47:58 -0500

    feat: add cantilever penalty and arch bonus - Cantilever penalty punishes blocks supported only horizontally. - Arch bonus reduces stress if a block is supported on both sides (compressive force).

--- src/main/java/graveler/action/UpdateStress.java
+++ src/main/java/graveler/action/UpdateStress.java
@@ -39,15 +39,35 @@ public record UpdateStress(BlockPos pos) implements Action {
     if (!point.isStressAware()) {
       return 0;
     }
+    int stress = point.move(Direction.DOWN).stress();
+    for (Direction direction : Direction.Plane.HORIZONTAL) {
+      var neighbor = point.move(direction);
+      stress = Math.min(stress, neighbor.stress() + 1);
+    }
+    stress = Math.max(0, stress + cantileverPenalty(point));
+    stress = Math.max(0, stress - archBonus(point));
+    return stress;
+  }
 
-    PointedWorld below = point.move(Direction.DOWN);
-    int horizontalStress =
-        Direction.Plane.HORIZONTAL
-            .stream()
-            .mapToInt(direction -> point.move(direction).stress() + 1)
-            .min()
-            .orElse(Integer.MAX_VALUE);
+  private static int cantileverPenalty(PointedWorld point) {
+    if (!point.move(Direction.DOWN).blockState().isSolid()) {
+      return 1;
+    }
+    return 0;
+  }
 
-    return Math.min(horizontalStress, below.stress());
+  private static int archBonus(PointedWorld point) {
+    var result = 0;
+    var eastDown = point.move(Direction.EAST).move(Direction.DOWN);
+    var westDown = point.move(Direction.WEST).move(Direction.DOWN);
+    if (eastDown.blockState().isSolid() && westDown.blockState().isSolid()) {
+      result += 1;
+    }
+    var northDown = point.move(Direction.NORTH).move(Direction.DOWN);
+    var southDown = point.move(Direction.SOUTH).move(Direction.DOWN);
+    if (northDown.blockState().isSolid() && southDown.blockState().isSolid()) {
+      result += 1;
+    }
+    return result;
   }
 }
