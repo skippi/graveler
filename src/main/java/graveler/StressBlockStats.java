b590ccc23d0fdc555285990d2d117f3cb0da5203
skippi <jbtcao@gmail.com>
2026-05-28 20:16:08 -0500

    feat: set strength for torches and grass/dirt

--- src/main/java/graveler/StressBlockStats.java
+++ src/main/java/graveler/StressBlockStats.java
@@ -1,8 +1,6 @@
 package graveler;
 
-import java.util.Map;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.Identifier;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -11,6 +9,21 @@ public record StressBlockStats(int strength) {
   public static final int DEFAULT_STRENGTH = 8;
 
   public static StressBlockStats get(Block block) {
-    return new StressBlockStats(DEFAULT_STRENGTH);
+    return new StressBlockStats(strength(block));
+  }
+
+  private static int strength(Block block) {
+    if (isTorch(block)) {
+      return 1;
+    }
+    var blockTags = block.builtInRegistryHolder();
+    if (blockTags.is(BlockTags.DIRT) || blockTags.is(BlockTags.GRASS_BLOCKS)) {
+      return 4;
+    }
+    return DEFAULT_STRENGTH;
+  }
+
+  private static boolean isTorch(Block block) {
+    return BuiltInRegistries.BLOCK.getKey(block).getPath().contains("torch");
   }
 }
